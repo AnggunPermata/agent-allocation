1d2d30364adbab6b4a13c84e5bc5b87109af716f
Anggun Permata <anggunpermata.g789@gmail.com>
2021-09-29 15:57:50 +0700

    initiate authorization

---
+++ auth/authorization.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/anggunpermata/agent-allocation/constant"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func LogMiddlewares(e *echo.Echo) {
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `[${time_rfc3339}] ${status} ${method} ${host} ${path} ${latency_human}` + "\n",
+	}))
+}
+
+//This is a function to create a new token everytime agent login
+func CreateToken(userId int) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = int(userId)
+	claims["role"] = "agent"
+	claims["exp"] = time.Now().Add(time.Hour * 2).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(constant.SECRET_JWT))
+}
+
+func CreateCustomerToken(userId int) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = int(userId)
+	claims["role"] = "customer"
+	claims["exp"] = time.Now().Add(time.Hour * 2).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(constant.SECRET_JWT))
+}
+
+func ExtractTokenUserId(c echo.Context) (int, string) {
+	user := c.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		fmt.Println(claims)
+		agentId := int(claims["userId"].(float64))
+		role := fmt.Sprintf("%v", claims["role"])
+		return agentId, role
+	}
+	return 0, "a"
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/anggunpermata/agent-allocation
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
