44e52cb2d28cc4399436afcf33e2d09148621c18
Anggun Permata <anggunpermata.g789@gmail.com>
2021-10-11 21:55:10 +0700

    updating Procfile

--- config/config.go
+++ config/config.go
@@ -1,10 +1,12 @@
 package config
 
 import (
+	"log"
+	"os"
 	"strconv"
 
-	"github.com/anggunpermata/agent-allocation/constant"
 	"github.com/anggunpermata/agent-allocation/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -13,12 +15,24 @@ var DB *gorm.DB
 
 var PORT int
 
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func InitDB() {
 	// connectionString := "root:12345@tcp(172.17.0.1:3307)/qiscus?charset=utf8&parseTime=True&loc=Local"
 	// fmt.Println("constant.Configuration: ", constant.Configuration)
 	// fmt.Println(connectionString)
 	var err error
-	DB, err = gorm.Open(mysql.Open("root:12345@tcp(172.17.0.1:3307)/qiscus?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(goDotEnvVariable("DATABASE_URL")), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
@@ -27,7 +41,7 @@ func InitDB() {
 }
 
 func InitPort() {
-	PORT, _ = strconv.Atoi(constant.Configuration["PORT"])
+	PORT, _ = strconv.Atoi(goDotEnvVariable("PORT"))
 }
 
 func InitMigrate() {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
